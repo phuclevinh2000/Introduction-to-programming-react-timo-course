a25c3df1c9125af96fe961779548a173daaa88cf
Phuc Le <phuclevinh2000@gmail.com>
2021-09-26 14:59:10 +0300

    update on w31 assignment

--- w31-update-torijs-and-tori-css/src/Tori.js
+++ w31-update-torijs-and-tori-css/src/Tori.js
@@ -4,24 +4,33 @@ import "./Tori.css";
 const Tori = () => {
   return (
     <div className="tori">
-      <input type="text" value="Hakusana ja/tai postinumero" />
-      <select name="what">
-        <option id="1">Kaikki osastot</option>
-      </select>
-      <select name="where">
-        <option id="1">Koko Suomi</option>
-      </select>
-      <input type="checkbox" name="how" />
-      <span>Myydään</span>
-      <input type="checkbox" name="how" />
-      <span>Ostetaan</span>
-      <input type="checkbox" name="how" />
-      <span>Vuokrataan</span>
-      <input type="checkbox" name="how" />
-      <span>Halutaan vuokrata</span>
-      <input type="checkbox" name="how" />
-      <span>Annetaan Tallenna haku</span>
-      <button>Hae</button>
+      <div className="head">
+        <input type="text" value="Hakusana ja/tai postinumero" />
+        <select name="what">
+          <option id="1">Kaikki osastot</option>
+        </select>
+        <select name="where">
+          <option id="1">Koko Suomi</option>
+        </select>
+      </div>
+      <div className="body">
+        <div className="left">
+          <input type="checkbox" name="how" />
+          <span>Myydään</span>
+          <input type="checkbox" name="how" />
+          <span>Ostetaan</span>
+          <input type="checkbox" name="how" />
+          <span>Vuokrataan</span>
+          <input type="checkbox" name="how" />
+          <span>Halutaan vuokrata</span>
+          <input type="checkbox" name="how" />
+          <span>Annetaan</span>
+        </div>
+        <div className="right">
+          <span >Tallenna haku</span>
+          <button>Hae</button>
+        </div>
+      </div>
     </div>
   );
 };
